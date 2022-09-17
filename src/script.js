248082bf697ff0dd88fb1b1e31378e7337e5157a
CactusPancake <luu.020989@gmail.com>
2022-09-17 02:53:33 +0100

    Current button working
    Search bar - working
    Time - working
    Current button - working

--- src/script.js
+++ src/script.js
@@ -1,3 +1,32 @@
+//------------------------------- Time -------------------------------
+let now = new Date();
+
+let timeElement = document.querySelector("#current-date");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let day = days[now.getDay()];
+
+if (minutes < 9) {
+  minutes = "0" + minutes;
+}
+
+if (hours < 9) {
+  hours = "0" + hours;
+}
+
+timeElement.innerHTML = `${day}  ${hours}:${minutes}`;
+
+//------------------------------- Weather -------------------------------
 function displayTemperature(response) {
   console.log(response);
   let temperatureElement = document.querySelector("#temperature");
@@ -18,7 +47,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
 }
-
+//------------------------------- Location Search -------------------------------
 function search(city) {
   let apiKey = "203fa770242fcd2b9555d832a88ea567";
 
@@ -36,4 +65,41 @@ function handleSubmit(event) {
 let form = document.querySelector("#location-search");
 form.addEventListener("submit", handleSubmit);
 
-search("New York");
+search("London");
+//------------------------------- Current Location Button -------------------------------
+function currentCity(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "91e4be9d3f0ce62462b88df7804804ae";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentCity);
+}
+
+let currentButton = document.querySelector("#current-btn");
+currentButton.addEventListener("click", getCurrentLocation);
+
+/*------------------------------- Celcius | Fahrenheit links -------------------------------
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahr);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", showCelc);
+
+function showFahr(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#temp");
+  temp.innerHTML = `66`;
+}
+
+function showCelc(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#temp");
+  temp.innerHTML = `20`;
+}
+*/
